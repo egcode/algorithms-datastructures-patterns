c40232da5fa2cd0fe444ee896db0408772ca3917
Eugene <xcode@example.com>
2021-12-27 13:32:39 -0800

    q

---
+++ Helpers/CPP/queue.cpp
@@ -0,0 +1,31 @@
+/*
+g++ queue.cpp -std=c++11;./a.out
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main(){
+
+    queue<int> q;
+
+    // push an element at the end of queue
+    q.push(1); // {1}
+    q.push(2); // {2, 1}
+    q.push(3); // {3, 2, 1}
+    q.push(4); // {4, 3, 2, 1}
+
+    // iterate till queue is not empty
+    while (!q.empty()){
+        // get the front element of queue
+        cout<<q.front()<<" ";
+        // remove the front element of queue
+        q.pop();
+    }
+    /*
+    1 2 3 4
+    */
+
+    return 0;
+}
