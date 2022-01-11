7f6a911f3162c2d5584bea15d8e4668e0e36b375
Eugene <xcode@example.com>
2022-01-10 20:03:06 -0800

    fenwickTree

--- Datastructures/CPP/fenwickTree.cpp
+++ Datastructures/CPP/fenwickTree.cpp
@@ -6,6 +6,57 @@ g++ fenwickTree.cpp -std=c++17;./a.out
 using namespace std;
 
 
+class BIT {
+    vector<int>bit;
+public:
+    BIT (int n=0) {
+        bit.assign(n+1, 0);
+    }
+    
+    void add(int i, int value) { 
+        while (i<bit.size()) {
+            bit[i] += value;
+            i += i & (-i);
+        }
+    }
+    
+    int sum(int i) {
+        int sum=0;
+        while (i>0) {
+            sum += bit[i];
+            i -= i & (-i);
+        }
+        return sum;
+    }
+    
+    int sumRange(int start, int end) {
+        return sum(end)-sum(start-1); // don't include start
+    }
+};
+
+// Num array has array that it supports with the help of Binary Indeded Tree
+class NumArray {
+    vector<int>nums;
+    BIT bit;
+public:
+    NumArray(vector<int>& nums) {
+        this->bit = BIT(nums.size());
+        this->nums = nums;
+        for (int i = 0; i < nums.size(); ++i)
+            bit.add(i+1, nums[i]);
+    }
+    
+    void update(int index, int val) {
+        int dif=val-nums[index];
+        bit.add(index+1, dif);
+        nums[index]=val;
+    }
+    
+    int sumRange(int left, int right) {
+        return bit.sumRange(left+1, right+1);
+    }
+};
+
 
 int main() {
 
@@ -22,9 +73,14 @@ int main() {
         [null, 9, null, 8]
     */
 
-    cout << endl << "-----------------------------" << endl; 
+    cout << endl << "-----------------------------" << endl;
+    vector<int> nums;
+    nums = {1, 3, 5};
+    auto numArray = new NumArray(nums);
 
-
+    cout << "Sum range1: " << numArray->sumRange(0, 2) << endl; // return 1 + 3 + 5 = 9
+    numArray->update(1, 2);
+    cout << "Sum range2: " << numArray->sumRange(0, 2) << endl; // return 1 + 2 + 5 = 8
 
     return 0;
 }
