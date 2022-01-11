44e0db09857a9dc9d91ab664d695ada1b54c5f70
Eugene <xcode@example.com>
2022-01-10 19:30:19 -0800

    cpp fenwick

---
+++ Datastructures/CPP/fenwickTree.cpp
@@ -0,0 +1,30 @@
+/*
+g++ fenwickTree.cpp -std=c++17;./a.out
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+int main() {
+
+    /*
+    Example: 
+        Range Sum Query - Mutable   
+            problem: https://leetcode.com/problems/range-sum-query-mutable/
+            github: https://github.com/egcode/algorithms-datastructures-patterns/blob/main/Patterns/LEETCODE/DataStructures/Tree/Binary%20Indexed%20Tree/Range%20Sum%20Query%20-%20Mutable.md
+
+        Input
+        ["NumArray", "sumRange", "update", "sumRange"]
+        [[[1, 3, 5]], [0, 2], [1, 2], [0, 2]]
+        Output
+        [null, 9, null, 8]
+    */
+
+    cout << endl << "-----------------------------" << endl; 
+
+
+
+    return 0;
+}
