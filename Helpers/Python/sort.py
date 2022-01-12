5eadf5fbdc36beb67369d9d2b1750a276dd58fef
Eugene <xcode@example.com>
2022-01-12 15:22:24 -0800

    sort python

--- Helpers/Python/sort.py
+++ Helpers/Python/sort.py
@@ -5,6 +5,14 @@ print("Sorted Tuple: " + str(t)) # Sorted Tuple: [[0, 1], [1, 4], [4, 5]]
 
 print(60*"-")
 
+# Sort By tuple two elements
+envelopes=[[4,2],[4,7],[0,1]]
+envelopes.sort(key=lambda tup:(tup[0], tup[1]))
+print("Sorted Tuple: " + str(envelopes)) # Sorted Tuple: [[0, 1], [1, 4], [4, 5]]
+
+print(60*"-")
+
+
 # Sort string
 some_str = "defbgca"
 str_sorted="".join(sorted(some_str))
