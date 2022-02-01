3c99d6817c7b1c36739ef556414d3b198a0ae0bb
Eugene <xcode@example.com>
2022-02-01 09:52:06 -0800

    LRUCache

---
+++ Datastructures/Python/LRUCache.py
@@ -0,0 +1,88 @@
+class Node:
+    def __init__(self, val, key):
+        self.key=key
+        self.val=val
+        self.prev=None
+        self.next=None
+        
+    
+class LRUCache:
+        
+    def __init__(self, capacity: int):
+        self.dic={}
+        self.cap=capacity
+        self.count=0
+        self.head=Node(-1,-1)
+        self.tail=Node(-1,-1)
+        self.head.next=self.tail
+        self.tail.pres=self.head
+
+    def add(self, node):
+        node.prev=self.head
+        node.next=self.head.next
+        
+        self.head.next.prev=node
+        self.head.next=node
+        
+    def remove(self,node):
+        prev=node.prev
+        nxt=node.next
+        node.prev=None
+        node.next=None
+        
+        prev.next=nxt
+        nxt.prev=prev
+        
+    def cut_tail(self):
+        to_del=self.tail.prev
+        self.remove(to_del)
+        del self.dic[to_del.key]
+        self.count -= 1
+
+
+
+    def get(self, key: int) -> int:
+        '''
+        Get Value if exists
+        '''
+        if key in self.dic.keys():
+            node=self.dic[key]
+            self.remove(node)
+            self.add(node)
+            return node.val
+        else:
+            return -1
+        
+
+    def put(self, key: int, value: int) -> None:
+        '''
+        Put Value, remove least used one
+        '''
+        if key in self.dic.keys():
+            node=self.dic[key]
+            self.remove(node)
+            node.val=value
+            self.add(node)
+        else:
+            new_node=Node(value, key)
+            self.dic[key]=new_node
+            self.add(new_node)
+            self.count += 1            
+            if self.count>self.cap:
+                self.cut_tail()
+
+
+'''
+Example from 146. LRU Cache in leetcode
+'''
+lRUCache = LRUCache(2)
+
+print(lRUCache.put(1, 1)); # cache is {1=1}
+print(lRUCache.put(2, 2)); # cache is {1=1, 2=2}
+print(lRUCache.get(1));    # return 1
+print(lRUCache.put(3, 3)); # LRU key was 2, evicts key 2, cache is {1=1, 3=3}
+print(lRUCache.get(2));    # returns -1 (not found)
+print(lRUCache.put(4, 4)); # LRU key was 1, evicts key 1, cache is {4=4, 3=3}
+print(lRUCache.get(1));    # return -1 (not found)
+print(lRUCache.get(3));    # return 3
+print(lRUCache.get(4));    # return 4
